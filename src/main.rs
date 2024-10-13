581ebe797ab9bb738367ff820df50e519bedc756
IwantHAPPINESS <magomadovimran142@gmail.com>
2024-10-13 15:42:04 +0300

    add linux dir

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,34 @@ use rusqlite::Connection;
 
 const DB: &str = "password.db";
 
+#[cfg(target_os = "linux")]
+pub fn only_linux() -> Connection {
+    let home_dir = match std::env::var("HOME") {
+        Ok(path) => path,
+        Err(e) => panic!("Could not find HOME directory: {e}"),
+    };
+
+    let path = format!("{home_dir}/Documents/{DB}");
+    match Connection::open(path) {
+        Ok(e) => e,
+        Err(e) => panic!("Error: {e}"),
+    }
+}
+
+// Todo
+#[cfg(target_os = "windows")]
+pub fn only_windows() -> Connection {
+    Connection::open(DB).unwrap()
+}
+
 fn main() {
     let cli = Cli::run();
 
-    let conn = Connection::open(DB).unwrap();
+    #[cfg(target_os = "linux")]
+    let conn = only_linux();
+
+    #[cfg(target_os = "windows")]
+    let conn = only_windows();
 
     create_table(&conn).unwrap();
 
