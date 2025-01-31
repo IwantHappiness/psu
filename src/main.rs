98ccd49bc6f3ab1409277f9325f97e5bc4ed8ea7
IwantHAPPINESS <magomadovimran142@gmail.com>
2025-01-31 14:49:30 +0300

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -5,60 +5,60 @@ const DB: &str = "password.db";
 
 #[cfg(target_os = "linux")]
 pub fn only_linux() -> Connection {
-    use std::{fs, path::Path};
+	use std::{fs, path::Path};
 
-    let home_dir = match std::env::var("HOME") {
-        Ok(path) => path,
-        Err(e) => panic!("HOME : {e}"),
-    };
+	let home_dir = match std::env::var("HOME") {
+		Ok(path) => path,
+		Err(e) => panic!("HOME : {e}"),
+	};
 
-    let path = format!("{home_dir}/Documents");
+	let path = format!("{home_dir}/Documents");
 
-    if !Path::new(&path).try_exists().unwrap() {
-        fs::create_dir(&path).expect("Failed create dir Documents");
-    }
+	if !Path::new(&path).try_exists().unwrap() {
+		fs::create_dir(&path).expect("Failed create dir Documents");
+	}
 
-    let path = format!("{}/{DB}", path);
-    Connection::open(path).expect("Failed to open the database")
+	Connection::open(format!("{}/{DB}", path))
+		.expect("Failed to open the database")
 }
 
 #[cfg(target_os = "windows")]
 pub fn only_windows() -> Connection {
-    use directories::UserDirs;
-    use std::fs::PathBuf;
+	use directories::UserDirs;
+	use std::path::PathBuf;
 
-    if let Some(doc) = UserDirs::new() {
-        let doc_path = doc.document_dir().unwrap();
-        let path = PathBuf::from(doc_path).join(DB);
+	if let Some(doc) = UserDirs::new() {
+		let doc_path = doc.document_dir().unwrap();
+		let path = PathBuf::from(doc_path).join(DB);
 
-        Connection::open(path).unwrap()
-    } else {
-        panic!("Failed to open the database");
-    }
+		return Connection::open(path).unwrap();
+	}
+
+	panic!("Failed to open the database");
 }
 
 fn main() {
-    let cli = Cli::run();
+	let cli = Cli::run();
 
-    #[cfg(target_os = "linux")]
-    let conn = only_linux();
+	#[cfg(target_os = "linux")]
+	let conn = only_linux();
 
-    #[cfg(target_os = "windows")]
-    let conn = only_windows();
+	#[cfg(target_os = "windows")]
+	let conn = only_windows();
 
-    create_table(&conn).unwrap();
+	create_table(&conn).unwrap();
 
-    match cli.command {
-        Commands::Add {
-            service,
-            login,
-            password,
-        } => insert(&conn, &service, &login, &password).unwrap(),
+	match cli.command {
+		Commands::Add {
+			service,
+			login,
+			password,
+		} => insert(&conn, &service, &login, &password).unwrap(),
 
-        Commands::Print { all, id } => print(&conn, all, id),
+		Commands::Print { all, id } => print(&conn, all, id),
 
-        Commands::Modify(p) => p.modify(&conn),
+		Commands::Modify(p) => p.modify(&conn),
 
-        Commands::Remove { id, all } => remove(&conn, id, all),
-    };
+		Commands::Remove { id, all } => remove(&conn, id, all),
+	};
 }
--- src/password.rs
+++ src/password.rs
@@ -5,35 +5,35 @@ use rusqlite::{params, Connection};
 
 #[derive(Debug, PartialEq, Clone, Parser)]
 pub struct Password {
-    pub id: u16,
-    pub service: String,
-    pub login: String,
-    pub password: String,
+	pub id: u16,
+	pub service: String,
+	pub login: String,
+	pub password: String,
 }
 
 impl Password {
-    pub fn modify(&self, conn: &Connection) {
-        let old_password = search_by_id(conn, self.id).unwrap();
+	pub fn modify(&self, conn: &Connection) {
+		let old_password = search_by_id(conn, self.id).unwrap();
 
-        conn.execute(
+		conn.execute(
             "UPDATE password SET service = ?1, login = ?2, password = ?3 WHERE id = ?4",
             params![self.service, self.login, self.password, self.id],
         )
         .unwrap();
-        println!(
-            "Old password: {} {} {} {}",
-            old_password.id.to_string().red(),
-            old_password.service.to_string().red(),
-            old_password.login.to_string().red(),
-            old_password.password.to_string().red()
-        );
+		println!(
+			"Old password: {} {} {} {}",
+			old_password.id.to_string().red(),
+			old_password.service.to_string().red(),
+			old_password.login.to_string().red(),
+			old_password.password.to_string().red()
+		);
 
-        println!(
-            "New password : {} {} {} {}",
-            self.id.to_string().green(),
-            self.service.to_string().green(),
-            self.login.to_string().green(),
-            self.password.to_string().green()
-        );
-    }
+		println!(
+			"New password : {} {} {} {}",
+			self.id.to_string().green(),
+			self.service.to_string().green(),
+			self.login.to_string().green(),
+			self.password.to_string().green()
+		);
+	}
 }
