5a889cc6fe4b0e616664178a04d852ec6117b7fe
IwantHappiness <iwanthappiness95@gmail.com>
2025-05-10 15:39:34 +0300

    feat(config): add config create

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use app::App;
 use color_eyre::Result;
+use config::Config;
+
 mod app;
+mod config;
 
 fn main() -> Result<()> {
+	if let Err(e) = Config::new().generate_conf() {
+		eprintln!("Error: {e}");
+	}
+
 	color_eyre::install()?;
 	let terminal = ratatui::init();
 	let app_result = App::new().run(terminal);
